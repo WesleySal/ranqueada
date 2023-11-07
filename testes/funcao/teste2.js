cf805f5e4b2270846c716cb6d069b05ca3180b22
WesleySal <128597560+WesleySal@users.noreply.github.com>
2023-11-07 13:43:49 -0300

    Delete testes directory

--- testes/funcao/teste2.js
+++
@@ -1,8 +0,0 @@
-torrar("pão de forma")
-torrar("pão integral")
-
-function torrar(pao){ //paâmetro
-    console.log("torrada feita " + pao)
-}
-
-console.log(pao)
--- testes/vetor/teste.js
+++
@@ -1,7 +0,0 @@
-let pokemon = ["pikachu", "charmander", "bulbasaur"]
-let timePokemon = [
-    ["Pikachu", "M", "level 1"],
-    ["Chamander", "M", "level 2"]
-]
-
-console.log(timePokemon)
